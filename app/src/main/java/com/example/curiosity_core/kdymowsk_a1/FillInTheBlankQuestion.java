7d9ddd1d5d3a8d4970bd40597ce1803bc701774b
kdymowski <kdymowsk@mines.edu>
2016-01-19 14:07:48 -0700

    First commit

---
+++ app/src/main/java/com/example/curiosity_core/kdymowsk_a1/FillInTheBlankQuestion.java
@@ -0,0 +1,13 @@
+package com.example.curiosity_core.kdymowsk_a1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FillInTheBlankQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill_in_the_blank_question);
+    }
+}
---
+++ app/src/main/java/com/example/curiosity_core/kdymowsk_a1/MultipleChoiceQuestion.java
@@ -0,0 +1,13 @@
+package com.example.curiosity_core.kdymowsk_a1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultipleChoiceQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiple_choice_question);
+    }
+}
